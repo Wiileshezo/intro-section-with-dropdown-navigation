37e1fc6bfd585583faf9fcb8d50bbedbb6644e02
Wiileshezo <wiileshezo@gmail.com>
2024-02-14 00:48:13 +0200

    make the home page responsive

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,13 +2,7 @@
 
 <template>
   <div class="container">
-    <div class="section1">
-      <img
-        src="../images/image-hero-mobile.png"
-        alt="image-hero"
-        class="hero-img"
-      />
-    </div>
+    <div class="section1"></div>
     <div class="section2">
       <h1>Make remote work</h1>
       <p>
@@ -32,7 +26,8 @@
   flex-direction: column;
   align-items: center;
   box-sizing: border-box;
-  padding-top: 5rem;
+  padding-top: 6rem;
+  margin: auto;
 }
 h1 {
   margin: 0;
@@ -53,6 +48,7 @@ button {
   padding: 0.7rem 2rem;
   font-size: 1rem;
   font-weight: 700;
+  margin-block-end: 3rem;
 }
 
 button:hover,
@@ -63,29 +59,111 @@ button:active {
 
 .section1 {
   display: flex;
-  /* background-image: url("../images/image-hero-mobile.png"); */
+  width: 99vw;
+  height: 45vh;
+  padding-block: 2rem 6rem;
+  background-image: url("../images/image-hero-desktop.png");
   background-repeat: no-repeat;
+  background-size: contain;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  margin: auto;
 }
 .section2 {
   display: flex;
+  width: unset;
   flex-direction: column;
   align-items: center;
   padding-inline: 2rem;
   text-align: center;
 }
-.hero-img {
-  width: 100vw;
-  height: 60vh;
-  padding-block: 1rem 3rem;
-}
 .imgs-client {
   display: flex;
   align-items: center;
   justify-content: center;
-  margin-block-start: 3rem;
+  margin: auto;
 }
 .imgs-client > img {
   padding: 0.5rem;
   width: 4rem;
+}
+@media screen and (max-width: 500px) {
+  .section1 {
+    background-image: url("../images/image-hero-mobile.png");
+  }
+}
+
+@media screen and (min-width: 600px) {
+  .container {
+    flex-direction: row-reverse;
+    align-items: flex-end;
+    padding-top: 5.1rem;
+    padding-inline: 3rem;
+  }
+  .section1 {
+    width: 50vw;
+    height: 60vh;
+  }
+  .section2 {
+    width: 50vw;
+    align-items: flex-start;
+    text-align: left;
+    padding-inline: 0 2rem;
+  }
+  h1 {
+    font-size: 3rem;
+  }
+}
+
+@media screen and (min-width: 1000px) {
+  .container {
+    padding-top: 7rem;
+  }
+  .section1 {
+    height: 60vh;
+  }
+  .section2 {
+    padding-inline: 3rem;
+  }
+
+  h1 {
+    font-size: 3.9rem;
+  }
+  p {
+    font-size: 1.2rem;
+  }
+  .imgs-client > img {
+    width: 5rem;
+  }
+}
+
+@media screen and (min-width: 1400px) {
+  .section1 {
+    width: 45vw;
+    height: 65vh;
+  }
+  .section2 {
+    width: 65vw;
+  }
+  h1 {
+    font-size: 6.5rem;
+    padding-inline-end: 2rem;
+  }
+  p {
+    font-size: 1.5rem;
+    line-height: 2rem;
+    padding-inline-end: 10rem;
+  }
+  button {
+    padding: 1rem 2rem;
+    font-size: 1.5rem;
+    margin-block-end: 6rem;
+    border-radius: 1rem;
+    border: 5px solid var(--Almost-Black);
+  }
+  .imgs-client > img {
+    width: 7rem;
+  }
 }
 </style>
